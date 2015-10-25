2c3b37427cab4f7e44f82e0127acfc916eb8acad
vire <viliam.elischer@gmail.com>
2015-10-25 11:25:14 +0100

    Removed code dup

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -60,14 +60,16 @@ class TaskModal extends Component {
   }
 
   _submitHandler() {
-    if (!this.state.dialogContent.trim()) {
+    const trimmedContent = this.state.dialogContent.trim();
+
+    if (!trimmedContent) {
       this.setState({
         errorText: `Cant't save empty task`,
       });
       return;
     }
     const res = this.props.task;
-    res.content = this.state.dialogContent.trim();
+    res.content = trimmedContent;
     this.props.onSubmit(res);
   }
 
