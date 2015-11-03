0acc9633b16cf3bf4d44b01552a463bdc15d92d9
Viliam Elischer <viliam.elischer@zoomint.com>
2015-11-03 08:09:53 +0100

    Added test for doubleClick on task

--- src/test/components/BoardTask.spec.js
+++ src/test/components/BoardTask.spec.js
@@ -2,26 +2,29 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import BoardTask from '../../components/BoardTask';
 
-function setup() {
+function setup(overrideProps) {
   const renderer = TestUtils.createRenderer();
   const identity = el => el;
 
-  const props = {
-    id: 42,
+  const props = Object.assign({
+    id: '42',
     content: '',
     dispatcher: {},
     isDragging: false,
     connectDragSource: identity,
-  };
+  }, overrideProps);
 
   const OriginalBoardTask = BoardTask.DecoratedComponent;
 
   renderer.render(<OriginalBoardTask {...props} />);
 
   const component = renderer.getRenderOutput();
+  const rendered = TestUtils.renderIntoDocument(<OriginalBoardTask {...props}/>);
 
   return {
     component,
+    rendered,
+    props,
   };
 }
 
@@ -39,4 +42,36 @@ describe('BoardTask', () => {
   it('should color `dot` and `content` based on storyGroup prop');
   it('should have style when isDragging:true');
   it('should not style when isDragging:false');
+  it('should handle doubleClick - fire action', () => {
+    const spyContent = {called: false, param: null};
+
+    const dispatch = (param) => {
+      spyContent.called = true;
+      spyContent.param = param;
+    };
+
+    const { rendered } = setup({
+      id: '456',
+      content: 'Blah',
+      sectionId: '101',
+      story: 'TEST',
+      dispatcher: {
+        dispatch,
+      },
+    });
+
+    const node = TestUtils.findRenderedDOMComponentWithClass(rendered, 'item');
+
+    TestUtils.Simulate.doubleClick(node);
+    expect(spyContent.called).to.be.true;
+    expect(spyContent.param).to.eql({
+      type: 'EDIT_TASK_CLICKED',
+      payload: {
+        id: '456',
+        content: 'Blah',
+        sectionId: '101',
+        story: 'TEST',
+      },
+    });
+  });
 });
