616a0112f79e722ecaa35938256c9214b6b6ea0d
vire <viliam.elischer@gmail.com>
2015-11-16 22:10:12 +0100

    Added sorting tasks by story(A-Z), priority(Z-A), id(A-Z)

---
+++ src/test/utils.spec.js
@@ -0,0 +1,113 @@
+import {sortTasks, compareStories, comparePriorities, compareTaskIDs }from '../utils/compare-story-helper';
+
+describe.skip('Priorities', () => {
+  it('are sorter from higher to lower', () => {
+    expect([{priority: 1}, {priority: 2}].sort(comparePriorities)
+    ).to.deep.equal([{priority: 2}, {priority: 1}]);
+  });
+
+  it('are sorter from defined to undefined', () => {
+    expect([{priority: 1}, {}].sort(comparePriorities)
+    ).to.deep.equal([{priority: 1 }, {}]);
+
+    expect([{}, {priority: 1}].sort(comparePriorities)
+    ).to.deep.equal([{priority: 1 }, {}]);
+  });
+
+  it('equal are not sorted', () => {
+    expect([{priority: 9, a: 1}, {priority: 9, a: 2}])
+      .to.deep.equal([{priority: 9, a: 1}, {priority: 9, a: 2}]);
+  });
+});
+
+describe('Tasks', () => {
+  it('are sorted by story(A-Z), priority(Z-A), id(A-Z)', () => {
+    expect(sortTasks([{story: 'B'}, {story: 'A'}]))
+      .to.deep.equal([{story: 'A'},{story: 'B'}]);
+
+    expect(sortTasks([{story: 'B', priority: 4}, {story: 'B', priority: 5}]))
+      .to.deep.equal([{story: 'B', priority: 5}, {story: 'B', priority: 4}]);
+
+    expect(sortTasks([{story: 'B', priority: 4, id: 'Z'}, {story: 'B', priority: 4, id: 'A'}]))
+      .to.deep.equal([{story: 'B', priority: 4, id: 'A'}, {story: 'B', priority: 4, id: 'Z' }]);
+  });
+  it('are sorted story (ASC), priority(DESC), id(ASC)', () => {
+    const tasks = [
+      {
+        id: '4',
+        story: '2',
+        priority: 7,
+      }, {
+        id: '5',
+        story: '2',
+        priority: 8,
+      }, {
+        id: '6',
+        story: '1',
+        priority: 2,
+      }, {
+        id: '3',
+        story: '1',
+        priority: 4,
+      },
+    ];
+    const sortedTasks = sortTasks(tasks);
+    expect(sortedTasks).to.deep.equal([
+      {
+        id: '3',
+        story: '1',
+        priority: 4,
+      }, {
+        id: '6',
+        story: '1',
+        priority: 2,
+      }, {
+        id: '5',
+        story: '2',
+        priority: 8,
+      }, {
+        id: '4',
+        story: '2',
+        priority: 7,
+      },
+    ]);
+  });
+});
+
+describe.skip('Stories', () => {
+  it('are sorted A-Z', () => {
+    expect([{story: 'B'}, {story: 'A'}].sort(compareStories))
+      .to.deep.equal([{story: 'A'}, {story: 'B'}]);
+  });
+
+  it('are sorter from defined to undefined', () => {
+    expect([{}, {story: 'A'}].sort(compareStories))
+      .to.deep.equal([{story: 'A'}, {}]);
+    expect([{story: 'A'}, {}].sort(compareStories))
+      .to.deep.equal([{story: 'A'}, {}]);
+  });
+
+  it('equal are not sorted', () => {
+    expect([{story: 'A', anotherKey: 'B'}, {story: 'A', anotherKey: 'A'}].sort(compareStories))
+      .to.deep.equal([{story: 'A', anotherKey: 'B'}, {story: 'A', anotherKey: 'A'}]);
+  });
+});
+
+describe.skip('IDs', () => {
+  it('are sorted A-Z', () => {
+    expect([{id: 'B'}, {id: 'A'}].sort(compareTaskIDs))
+      .to.deep.equal([{id: 'A'}, {id: 'B'}]);
+  });
+
+  it('are sorter from defined to undefined', () => {
+    expect([{}, {id: 'A'}].sort(compareTaskIDs))
+      .to.deep.equal([{id: 'A'}, {}]);
+    expect([{id: 'A'}, {}].sort(compareTaskIDs))
+      .to.deep.equal([{id: 'A'}, {}]);
+  });
+
+  it('equal are not sorted', () => {
+    expect([{id: 'A', anotherKey: 'B'}, {id: 'A', anotherKey: 'A'}].sort(compareTaskIDs))
+      .to.deep.equal([{id: 'A', anotherKey: 'B'}, {id: 'A', anotherKey: 'A'}]);
+  });
+});
---
+++ src/utils/compare-story-helper.js
@@ -0,0 +1,73 @@
+export const comparePriorities = (taskA, taskB) => {
+  if (typeof taskA.priority === 'undefined') {
+    return 1;
+  }
+
+  if (typeof taskA.priority === 'undefined') {
+    return -1;
+  }
+
+  if (taskA.priority < taskB.priority ) {
+    return 1;
+  }
+
+  if (taskA.priority > taskB.priority) {
+    return -1;
+  }
+
+  return 0;
+};
+
+export const compareStories = (taskA, taskB) => {
+  if (typeof taskA.story === 'undefined') {
+    return 1;
+  }
+
+  if (typeof taskA.story === 'undefined') {
+    return -1;
+  }
+
+  if (taskA.story < taskB.story ) {
+    return -1;
+  }
+
+  if (taskA.story > taskB.story) {
+    return 1;
+  }
+
+  return 0;
+};
+
+export const compareTaskIDs = (taskA, taskB) => {
+  if (typeof taskA.id === 'undefined') {
+    return 1;
+  }
+
+  if (typeof taskA.id === 'undefined') {
+    return -1;
+  }
+
+  if (taskA.id < taskB.id ) {
+    return -1;
+  }
+
+  if (taskA.id > taskB.id) {
+    return 1;
+  }
+
+  return 0;
+};
+
+export const sortTasks = (tasks) => {
+  return tasks.sort((taskA, taskB) => {
+    if (compareStories(taskA, taskB) !== 0) {
+      return compareStories(taskA, taskB); // -1 or 1
+    }
+
+    if (comparePriorities(taskA, taskB) !== 0 ) {
+      return comparePriorities(taskA, taskB);
+    }
+
+    return compareTaskIDs(taskA, taskB);
+  });
+};
