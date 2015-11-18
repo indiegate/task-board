7bc9374933c0ee83d1e5475755558b166cb29891
vire <viliam.elischer@gmail.com>
2015-11-18 22:16:36 +0100

    Updated path to babel-polyfill in karma

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function(config) {
     frameworks: ['mocha', 'chai'],
     files: [
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
-      './node_modules/babel-core/browser-polyfill.js',
+      './node_modules/babel-polyfill/dist/polyfill.js',
       'src/test/**/*.spec.*',
     ],
     webpack: {
